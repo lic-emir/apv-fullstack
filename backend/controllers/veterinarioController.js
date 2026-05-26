e9ccfb22dec05b08d2a75af0427bbd9e7996b670
Emir <emir.green.10@gmail.com>
2026-05-26 12:15:15 -0300

    feat: confirm user using the token passed as an argument in the URL

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -22,5 +22,22 @@ const registrar = async (req, res) => {
 const perfil = (req, res) => {
   res.json({msg: "mostrando perfil"});
 }
+const confirmar = async (req, res) => {
+  const {token} = req.params;
+  const usuarioConfirmar = await Veterinario.findOne({token});
+  if (!usuarioConfirmar) {
+    const error = new Error("Token no valido");
+    return res.status(404).json({msg: error.message});
+  }
+  try {
+    usuarioConfirmar.token = null;
+    usuarioConfirmar.confirmado = true;
+    await usuarioConfirmar.save();
+    res.json({msg: "Usuario confirmado correctamente"});  
+  } catch (error) {
+    console.error(error)
+  }
+  
+}
 
-export {registrar, perfil}
+export {registrar, perfil, confirmar}
