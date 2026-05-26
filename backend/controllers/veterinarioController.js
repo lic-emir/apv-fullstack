f2fbf4240d3295dd787db9cbf335e42f19061d57
Emir <emir.green.10@gmail.com>
2026-05-26 10:49:09 -0300

    feat: verify if user exists

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -1,6 +1,23 @@
+import Veterinario from "../models/Veterinario.js";
 
-const registrar = (req, res) => {
-  res.json({msg: "registrando usuario"});
+const registrar = async (req, res) => {
+  const {email} = req.body;
+  
+  //prevenir usuarios duplicados
+  const existeUsuario = await Veterinario.findOne({email});
+  if (existeUsuario) {
+    const error = new Error('Usuario ya registrado');
+    return res.status(400).json({msg: error.message})
+  }
+
+  try {
+    //guardar un nuevo veterinario
+    const veterinario = new Veterinario(req.body);
+    const veterinarioGuardado = await veterinario.save()
+    res.json(veterinarioGuardado);
+  } catch (error) {
+    console.error(error)
+  }
 }
 const perfil = (req, res) => {
   res.json({msg: "mostrando perfil"});
