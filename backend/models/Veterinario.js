f1105446d0101f050a5710ee7b8469dc904a2608
Emir <emir.green.10@gmail.com>
2026-05-28 09:49:52 -0300

    fix: change next for return

--- backend/models/Veterinario.js
+++ backend/models/Veterinario.js
@@ -37,9 +37,9 @@ const veterinarioSchema = mongoose.Schema({
   }
 });
 
-veterinarioSchema.pre('save', async function(next){
+veterinarioSchema.pre('save', async function(){
   if (!this.isModified('password')) {
-    next();
+    return;
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
