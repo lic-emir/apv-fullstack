1df9643595000ea96963eb745246003067e7b6fd
Emir <emir.green.10@gmail.com>
2026-05-29 12:35:42 -0300

    feat: A new route olvide-password was added and the functions that allow changing the password were associated with this route.

--- backend/routes/veterinarioRoutes.js
+++ backend/routes/veterinarioRoutes.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import checkAuth from '../middleware/authMiddleware.js';
-import { registrar, perfil, confirmar, autenticar } from '../controllers/veterinarioController.js';
+import { registrar, perfil, confirmar, autenticar, olvidePassword, comprobarToken, nuevoPassword } from '../controllers/veterinarioController.js';
 const router = express.Router();
 
+//area publica
 router.post("/", registrar);
 router.get("/confirmar/:token", confirmar);
 router.post("/login", autenticar);
+router.post("/olvide-password", olvidePassword);
+/*router.get("/olvide-password/:token", comprobarToken);
+router.post("/olvide-password/:token", nuevoPassword);*/
+router.route("/olvide-password/:token").get(comprobarToken).post(nuevoPassword);
+
+//area privada
 router.get("/perfil",checkAuth, perfil);
 
 export default router;
