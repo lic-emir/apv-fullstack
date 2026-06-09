606b1bbce29a66d07c52907750767e4b246f2b14
Emir <emir.green.10@gmail.com>
2026-06-09 11:48:07 -0300

    feat: HTML and styles completed

--- frontend/src/paginas/OlvidePassword.jsx
+++ frontend/src/paginas/OlvidePassword.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const OlvidePassword = () => {
   return (
     <>
-      <h1>Desde Olvide Password</h1>
+      <div>
+        <h1 className="text-indigo-600 font-black text-6xl">Recupera tu acceso y no pierdad <span className="text-black">tus pacientes.</span></h1>
+      </div>
+      <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
+        <form>
+          <div className="my-5">
+            <label htmlFor="email" className="uppercase text-gray-600 block text-xl font-bold">Email</label>
+            <input type="email" placeholder="Email de registro" id="email" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"/>
+          </div>
+          <input type="submit" value="Enviar instrucciones" className="bg-indigo-600 w-full py-3 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto md:px-10"/>
+        </form>
+        <nav className="mt-10 lg:flex lg:justify-between">
+          <Link to="/" className="block text-center my-5 text-gray-500">¿Ya tienes una cuenta? Inicia Sesión</Link>
+          <Link to="/registrar" className="block text-center my-5 text-gray-500">¿No tienes una cuenta? Registrate</Link>
+        </nav>
+      </div>
     </>
   )
 }
