0c2a6c750e0f01746ba5c62d78e0544da23ff3e9
Emir <emir.green.10@gmail.com>
2026-06-11 11:33:13 -0300

    feat: Allow sending of registration data between different domains using CORS, via Axios

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import conectarDB from './config/db.js';
 import veterinarioRoutes from './routes/veterinarioRoutes.js';
 import pacienteRoutes from './routes/pacienteRoutes.js';
@@ -11,6 +12,18 @@ app.use(express.json());
 
 conectarDB();
 
+const dominiosPermitidos = ['http://localhost:5173'];
+const corsOptions = {
+  origin: function(origin, callback){
+    if (dominiosPermitidos.indexOf(origin) !== -1) {
+      callback(null, true)
+    }else{
+      callback(new Error('No permitido por CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions));
 app.use("/api/veterinarios", veterinarioRoutes);
 app.use("/api/pacientes", pacienteRoutes);
 
