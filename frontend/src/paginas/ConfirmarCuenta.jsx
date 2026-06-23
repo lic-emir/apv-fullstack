fac6e9dfe4bc1dd893ce40cce03a0bef09db9557
Emir <emir.green.10@gmail.com>
2026-06-23 11:38:34 -0300

    feat: Display successful or unsuccessful validation depending on the token.

--- frontend/src/paginas/ConfirmarCuenta.jsx
+++ frontend/src/paginas/ConfirmarCuenta.jsx
@@ -1,7 +1,47 @@
+import { useEffect, useState } from 'react';
+import {useParams, Link} from 'react-router-dom'
+import axios from 'axios';
+import Alerta from '../components/Alerta'
+
 const ConfirmarCuenta = () => {
+  const [cuentaConfirmada, setCuentaConfirmada] = useState(false);
+  const [cargando, setCargando] = useState(true);
+  const [alerta, setAlerta] = useState({})
+  const params = useParams();
+  const {id} = params;
+
+  useEffect(()=>{
+    const confirmarCuenta = async () => {
+      try {
+        const url = `http://localhost:4000/api/veterinarios/confirmar/${id}`;
+        const {data} = await axios(url);
+        setCuentaConfirmada(true);
+        setAlerta({msg: data.msg});
+      } catch (error) {
+        setAlerta({
+          msg: error.response.data.msg,
+          error: true
+        })
+      }
+      setCargando(false)
+    }
+    confirmarCuenta();
+  }, [id]);
+
   return (
     <>
-      <h1>Desde Confirmar Cuenta</h1>
+      <div>
+        <h1 className="text-indigo-600 font-black text-6xl">  
+          Confirma tu cuenta y comienza a administrar
+          <span className="text-black">Tus pacientes</span>
+        </h1>
+      </div>
+      <div className="mt-20 md:mt-5 shadow-lg px-5 rounded-xl bg-white">
+        {!cargando && <Alerta alerta={alerta}/>}
+        {cuentaConfirmada && (
+          <Link className='block text-center my-5 text-gray-500'>Inicia Sesión</Link>
+        )}
+      </div>
     </>
   )
 }
