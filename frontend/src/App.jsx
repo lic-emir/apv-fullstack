956a7304c2b2d097932c87efab65b12f5c09632e
Emir <emir.green.10@gmail.com>
2026-07-01 10:57:09 -0300

    feat: user authentication using createContext and useContext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,21 +5,24 @@ import Registrar from "./paginas/Registrar";
 import ConfirmarCuenta from "./paginas/ConfirmarCuenta";
 import OlvidePassword from "./paginas/OlvidePassword";
 import NuevoPassword from "./paginas/NuevoPassword";
+import { AuthProvider } from "./context/AuthProvider";
 
 function App() {
   
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AuthLayout/>}>
-          <Route index element={<Login/>}/>
-          <Route path="registrar" element={<Registrar/>}/>
-          <Route path="olvide-password" element={<OlvidePassword/>}/>
-          <Route path="olvide-password/:token" element={<NuevoPassword/>}/>
-          <Route path="confirmar/:id" element={<ConfirmarCuenta/>}/>
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<AuthLayout/>}>
+            <Route index element={<Login/>}/>
+            <Route path="registrar" element={<Registrar/>}/>
+            <Route path="olvide-password" element={<OlvidePassword/>}/>
+            <Route path="olvide-password/:token" element={<NuevoPassword/>}/>
+            <Route path="confirmar/:id" element={<ConfirmarCuenta/>}/>
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
